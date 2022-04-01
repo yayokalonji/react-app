6697faaa6a5e45163cb68ba17bf5c03a80f2ba1f
Jackson Asprilla <con_jasprilla@credicorpbank.com>
2022-04-01 17:00:17 -0500

    Hello world

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        {getGreeting(user)}
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -20,6 +21,22 @@ function App() {
       </header>
     </div>
   );
+}
+
+function formatName(user) {
+  return user.firstName + ' ' + user.lastName;
+}
+
+function getGreeting(user) {
+  if (user) {
+    return <h1>Hello, {formatName(user)}!</h1>;
+  }
+  return <h1>Hello, Stranger.</h1>;
+}
+
+const user = {
+  firstName: 'Cristian',
+  lastName: 'Castro'
 }
 
 export default App;
